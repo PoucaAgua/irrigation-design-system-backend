9ce6ce863a89d816b56dabb9b8d792b22736d539
Lucas Ravellys <57733801+ravellys@users.noreply.github.com>
2023-07-15 00:19:31 -0300

    refactor: remove test enpoint (#20)

--- irrigation_design_system_backend/infrastructure/api/v1/__init__.py
+++ irrigation_design_system_backend/infrastructure/api/v1/__init__.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
 
-from infrastructure.api.v1.controllers import app_test_controller, evapotranspiration, project_controller
+from infrastructure.api.v1.controllers import  evapotranspiration, project_controller
 
 router = APIRouter()
 
 
-router.include_router(app_test_controller.router, prefix="/test", tags=["Endpoint Test"])
 router.include_router(evapotranspiration.router, prefix="/evapotranspiration", tags=["Endpoint Evapotranspiration"])
 router.include_router(project_controller.router, prefix="/projects", tags=["Endpoint to manager projects"])
