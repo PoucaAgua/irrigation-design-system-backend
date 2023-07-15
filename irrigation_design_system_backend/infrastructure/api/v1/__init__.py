eefc416bc7de039e2b07ccb39ff67614e7947200
ravellys <ravellyspyrrho@gmail.com>
2023-07-15 00:27:00 -0300

    refactor: renaming evapotranspiration variable

--- irrigation_design_system_backend/infrastructure/api/v1/__init__.py
+++ irrigation_design_system_backend/infrastructure/api/v1/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from infrastructure.api.v1.controllers import  evapotranspiration, project_controller
+from infrastructure.api.v1.controllers import  evapotranspiration_controller, project_controller
 
 router = APIRouter()
 
 
-router.include_router(evapotranspiration.router, prefix="/evapotranspiration", tags=["Endpoint Evapotranspiration"])
+router.include_router(evapotranspiration_controller.router, prefix="/evapotranspiration", tags=["Endpoint Evapotranspiration"])
 router.include_router(project_controller.router, prefix="/projects", tags=["Endpoint to manager projects"])
