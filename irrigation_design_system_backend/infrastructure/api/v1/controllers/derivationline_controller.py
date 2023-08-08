8d49c98922b57639c078322ad8b9a0649b2f194b
Lucas Lira <lucasclira@outlook.com>
2023-08-08 13:42:53 -0300

    feat: create controller for derivation line

---
+++ irrigation_design_system_backend/infrastructure/api/v1/controllers/derivationline_controller.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+from infrastructure.api.v1.responses.derivationline import DerivationlineReponse
+from core.domain.entity.derivationline_entity import DerivationlineEntity
+from apps.derivationline.derivationline_calculate_service import DerivationlineService
+
+router = APIRouter()
+
+@router.post("/derivation_line", response_model=DerivationlineReponse)
+def derivationline(derivationline_entity: DerivationlineEntity):
+
+    derivation = DerivationlineService.calculate_derivationline_dimensions(derivationline_entity)
+    return DerivationlineReponse(value=derivation)
