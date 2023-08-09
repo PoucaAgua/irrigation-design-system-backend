885092bdca0d90adce261e59135ada3dc7d5b444
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-08-09 18:13:20 -0300

    Test of the calculation of pw and ps

---
+++ irrigation_design_system_backend/tests/unit/apps/percent_shaded_area/test_reference_percent_shaded_area_service.py
@@ -0,0 +1,47 @@
+from _decimal import Decimal
+from core.constants.math import MathConstants
+from apps.percent_shaded_area.percent_shaded_area_service import PercentShadedAreaService
+from core.domain.entity.percent_shaded_area_entity import (
+    PlantCanopyProjectionInputEntity,
+    PlantStripProjectionInputEntity
+)
+
+
+class TestPercentShadedAreaService:
+    error = Decimal("1e-2")
+
+    def test_calculate_by_plant_canopy_projection(self):
+        # Given
+        input_entity = PlantCanopyProjectionInputEntity(
+            diameter_projection=Decimal(7),
+            space_between_plants=Decimal(1.5),
+            space_between_rows=Decimal(2)
+        )
+        expected_result = Decimal(
+            MathConstants.PI * (input_entity.diameter_projection ** 2 / 4) /
+            (input_entity.space_between_rows * input_entity.space_between_plants)
+        )
+
+        # When
+        result = PercentShadedAreaService.calculate_by_plant_canopy_projection(input_entity)
+        print(result)
+
+        # Then
+        assert abs(result - expected_result) <= self.error
+
+    def test_calculate_by_plant_strip_projection(self):
+        # Given
+        input_entity = PlantStripProjectionInputEntity(
+            shaded_strip_plant=Decimal(4),
+            space_between_rows=Decimal(2)
+        )
+        expected_result = Decimal(input_entity.shaded_strip_plant / input_entity.space_between_rows)
+
+        # When
+        result = PercentShadedAreaService.calculate_by_plant_strip_projection(input_entity)
+        print(result)
+
+        # Then
+        assert abs(result - expected_result) <= self.error
+
+
