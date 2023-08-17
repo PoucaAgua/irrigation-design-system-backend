2b6cd172c6cf63af5699396d3a59ea8c2d10921b
Lucas Lira <lucasclira@outlook.com>
2023-08-17 17:27:34 -0300

    test: unittest to derivationline service

---
+++ irrigation_design_system_backend/tests/unit/apps/derivationline/test_derivationline_service.py
@@ -0,0 +1,24 @@
+from _decimal import Decimal
+
+from apps.derivationline.derivationline_calculate_service import DerivationlineService
+from core.domain.entity.derivationline_entity import DerivationlineEntity
+
+class TestDerivationlineService:
+
+  def test_calculate_diameter():
+
+    error = 0
+
+    test_derivationline_entity = DerivationlineEntity(
+      friction_factor=Decimal(7),
+      flow=Decimal(25.0),
+      load_loss=Decimal(20.0),
+      pipe_length=Decimal(30.0),
+      )   
+    expected_result = float(35.0)
+
+    result = DerivationlineService.calculate_derivationline_dimensions(test_derivationline_entity)
+    print(result)
+
+    assert (expected_result - result) == error
+    print(f'_____Test_Passed____\n')
