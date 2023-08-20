04c09f3d8d9dfdb967d08d136963c4dbd58415b1
Evellyn Alexandra <Evellyn Alexandra>
2023-08-20 01:01:00 -0300

    update

--- irrigation_design_system_backend/apps/evapotranspiration/reference_evapotranspiration_service.py
+++ irrigation_design_system_backend/apps/evapotranspiration/reference_evapotranspiration_service.py
@@ -1,15 +1,11 @@
 from _decimal import Decimal
-
-
-
 from core.constants.evapotranspiration import ReferenceEvapotranspirationConstants
-from core.domain.entity.evapotranspiration_entity import  EToHargravesSamaniInputyEntity, EToPenmanMonteithInputyEntity,EToBlanneyCriddleInputyEntity
-import pandas as pd
+from core.domain.entity.evapotranspiration_entity import EToHargravesSamaniInputyEntity, EToPenmanMonteithInputyEntity,EToBlanneyCriddleInputyEntity
 from .funcitions_to_reference_evapotranspiration import (calculate_temperature_media, calculate_radiation_Hargreaves_Samani, calculate_radiation_blaney_criddle, calculate_vapor_saturation_pressure, calculate_vapor_current_pressure, calculate_declivity_curve_pressure_vapor, calculate_atmospheric_pressure, calculate_psychrometric_constant)
 
 parameters_hargraves_samani = ReferenceEvapotranspirationConstants.parameters_hargraves_samani
 parameters_blaney_cridlle = ReferenceEvapotranspirationConstants.parameters_blaney_cridlle
-parameters_penman_monteith = ReferenceEvapotranspirationConstants.parameters_penman_monteith
+# parameters_penman_monteith = ReferenceEvapotranspirationConstants.parameters_penman_monteith
 
 
 
@@ -49,5 +45,5 @@ class ReferenceEvapotranspirationService:
         u2 = eto_entity.wind_speed
         g = eto_entity.ground_heat
         rn = eto_entity.daily_radiation
-        # .
+        # . 
         return ((0.408* declivity_curve_pressure_vapor * (rn - g) + (psychrometric_constant * 900 * u2 * (es - ea) / Tmed + 273)) / (declivity_curve_pressure_vapor + psychrometric_constant * (1 + 0.34 * u2) ))
