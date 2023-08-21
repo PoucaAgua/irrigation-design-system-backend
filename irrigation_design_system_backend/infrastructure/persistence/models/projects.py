102793a0505d69e1d85f0f1d7d40532317635847
evandrosystems <evandrosystems@gmail.com>
2023-08-21 20:08:33 -0300

    feat: Model of the project, Lateral line and Derivation line

--- irrigation_design_system_backend/infrastructure/persistence/models/projects.py
+++ irrigation_design_system_backend/infrastructure/persistence/models/projects.py
@@ -10,14 +10,16 @@ class Project(Base):
     id = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
     user_id = Column(BigInteger, index=True)
     group_id = Column(String(100))
+    description = Column(String(250))
     status = Column(String(100))
     crop = Column(String(100))
-    irn_max = Column(DECIMAL(10, 2), default=0.00)
-    etc = Column(DECIMAL(10, 2), default=0.00)
-    itn = Column(DECIMAL(10, 2), default=0.00)
+    maximum_actual_irrigation_required = Column(DECIMAL(10, 2), default=0.00)
+    crop_evapotranspiration = Column(DECIMAL(10, 2), default=0.00)
+    total_irrigation_required = Column(DECIMAL(10, 2), default=0.00)
 
     # Foreign Keys
-    lateral = relationship('LateralLine', back_populates='project', uselist=True)
+    lateral_line = relationship('LateralLine', back_populates='project', uselist=True)
+    derivation_line = relationship('DerivationLine', back_populates='project', uselist=True)
 
 
 class LateralLine(Base):
@@ -35,13 +37,14 @@ class LateralLine(Base):
     type = Column(Enum('with_plc', 'without_plc'))
 
     # Foreign Keys
-    project = relationship('Project', back_populates='lateral', uselist=False)
+    project = relationship('Project', back_populates='lateral_line', uselist=False)
 
 
 class DerivationLine(Base):
     __tablename__ = "derivation_lines"
 
     id = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
+    project_id = Column(BigInteger, ForeignKey('projects.id'), index=True)
     pipe_type = Column(String(250))
     inlet_pressure = Column(DECIMAL(10, 2))
     length = Column(JSON)
@@ -49,3 +52,5 @@ class DerivationLine(Base):
     localized_loss = Column(String(250))
     type = Column(Enum('with_plc', 'without_plc'))
 
+    # Foreign Keys
+    project = relationship('Project', back_populates='derivation_line', uselist=False)
