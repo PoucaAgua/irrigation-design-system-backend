e02ce97717d899cfc0b4a5a8ab39f270b3a40a68
Lucas Lira <lucasclira@outlook.com>
2023-08-31 17:49:46 -0300

    refactor: create path for commercial diameter file

--- irrigation_design_system_backend/apps/file_reader/source_table.py
+++ irrigation_design_system_backend/apps/file_reader/source_table.py
@@ -1,8 +1,12 @@
 from pandas import read_csv
+from pathlib import Path
 
 class SourceTable:
 
   def reference_table_reading(path):
-    df = read_csv(path)
+
+    p = Path(path)
+
+    df = read_csv(p)
     return df
   
