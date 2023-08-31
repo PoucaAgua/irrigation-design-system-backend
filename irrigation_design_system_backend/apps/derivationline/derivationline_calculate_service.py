830fb3793e1539c2abc2e77ae651edbe2ec5cb02
Lucas Lira <lucasclira@outlook.com>
2023-08-31 16:57:51 -0300

    refactor:fixed the use of variable names

--- irrigation_design_system_backend/apps/derivationline/derivationline_calculate_service.py
+++ irrigation_design_system_backend/apps/derivationline/derivationline_calculate_service.py
@@ -4,18 +4,18 @@ from core.constants.derivationline import DerivationlineConstants
 from core.domain.entity.derivationline_entity import DerivationlineEntity
 from apps.consult_diameter.consult_nominal_diameter import ConsultNominalDiameterTable
 
+
 class DerivationlineService:
 
-  @staticmethod
-  def calculate_derivationline_dimensions(derivationline_entity: DerivationlineEntity):
+    @staticmethod
+    def calculate_derivationline_dimensions(derivationline_entity: DerivationlineEntity):
+        constant_derivationline = DerivationlineConstants.parameters_derivationline
 
-    constant_derivationline = DerivationlineConstants.parameters_derivationline
+        f = derivationline_entity.friction_factor
+        q = derivationline_entity.flow
+        hf = derivationline_entity.load_loss
+        lv = derivationline_entity.pipe_length
 
-    f = derivationline_entity.friction_factor
-    Q = derivationline_entity.flow
-    hf = derivationline_entity.load_loss
-    Lv = derivationline_entity.pipe_length
+        theoretical_dimensions = (((constant_derivationline * f * (q ** 2)) / hf) * lv) ** Decimal(0.2)
 
-    theorical_dimensions = (((constant_derivationline * f * ((Q)**2)) / hf) * Lv)**Decimal(0.2)
-
-    return (ConsultNominalDiameterTable.nominal_diameter(float(theorical_dimensions)))
+        return ConsultNominalDiameterTable.nominal_diameter(float(theoretical_dimensions))
