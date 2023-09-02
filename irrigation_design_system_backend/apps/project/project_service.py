c5eccbbea482cb438f1077274e140724fad30713
evandrosystems <evandrosystems@gmail.com>
2023-09-02 03:11:46 -0300

    feature: create the upsert endpoint to project, lateral_line and derivation_lite

--- irrigation_design_system_backend/apps/project/project_service.py
+++ irrigation_design_system_backend/apps/project/project_service.py
@@ -1,5 +1,6 @@
-from core.domain.entity.project_entity import ProjectEntity
-from infrastructure.persistence.repository.projects_repository import ProjectRepository
+from core.domain.entity.project_entity import ProjectEntity, DerivationLineEntity, LateralLineEntity
+from infrastructure.persistence.repository.projects_repository import ProjectRepository, DerivationLineRepository
+from infrastructure.persistence.repository.projects_repository import LateralLineRepository
 
 
 class ProjectService:
@@ -7,4 +8,20 @@ class ProjectService:
 
     @classmethod
     def upsert_project(cls, entity: ProjectEntity):
+        cls.repository.upsert(entity)
+
+
+class DerivationLineService:
+    repository = DerivationLineRepository()
+
+    @classmethod
+    def upsert_derivation_line(cls, entity: DerivationLineEntity):
+        cls.repository.upsert(entity)
+
+
+class LateralLineService:
+    repository = LateralLineRepository()
+
+    @classmethod
+    def upsert_lateral_line(cls, entity: LateralLineEntity):
         cls.repository.upsert(entity)
--- irrigation_design_system_backend/infrastructure/api/v1/controllers/project_controller.py
+++ irrigation_design_system_backend/infrastructure/api/v1/controllers/project_controller.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-from apps.project.project_service import ProjectService
-from core.domain.entity.project_entity import ProjectEntity
+from apps.project.project_service import ProjectService, DerivationLineService, LateralLineService
+from core.domain.entity.project_entity import ProjectEntity, DerivationLineEntity, LateralLineEntity
 from infrastructure.api.v1.responses.project import ProjectResponse
 
 router = APIRouter()
@@ -10,4 +10,16 @@ router = APIRouter()
 @router.post("/projects", response_model=ProjectResponse)
 def projects(project_entity: ProjectEntity):
     ProjectService.upsert_project(project_entity)
+    return ProjectResponse()
+
+
+@router.post("/derivation_line", response_model=ProjectResponse)
+def derivation(derivation_entity: DerivationLineEntity):
+    DerivationLineService.upsert_derivation_line(derivation_entity)
+    return ProjectResponse()
+
+
+@router.post("/lateral_line", response_model=ProjectResponse)
+def lateral_line(lateral_line_entity: LateralLineEntity):
+    LateralLineService.upsert_lateral_line(lateral_line_entity)
     return ProjectResponse()
