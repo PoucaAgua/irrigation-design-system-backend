464baa53a508033b4115084ad27b1dd08ff83eea
Lucas Lira <lucasclira@outlook.com>
2023-09-06 16:59:10 -0300

    feat: new calculation of diameter and load loss

--- irrigation_design_system_backend/apps/hydraulic_design/derivationline/derivationline_calculate_service.py
+++ irrigation_design_system_backend/apps/hydraulic_design/derivationline/derivationline_calculate_service.py
@@ -1,21 +1,43 @@
 from _decimal import Decimal
 
-from core.constants.derivationline import DerivationlineConstants
-from core.domain.entity.derivationline_entity import DerivationlineEntity
+from core.constants.hydraulic_design import HydraulicConstants
+from core.constants.math import MathConstants
+from core.domain.entity.derivationline_entity import DerivationlineDiameterEntity, DerivationlineLoadLoassEntity
 from apps.consult_diameter.consult_nominal_diameter import ConsultNominalDiameterTable
+from apps.hydraulic_design.aux_calculations.aux_calculation import AuxCalculationHydraulicDesign
 
 
 class DerivationlineService:
 
     @staticmethod
-    def calculate_derivationline_dimensions(derivationline_entity: DerivationlineEntity):
-        constant_derivationline = DerivationlineConstants.parameters_derivationline
+    def calculate_derivationline_dimensions(diameter_entity: DerivationlineDiameterEntity) -> Decimal:
+        
+        pi = MathConstants.PI
 
-        f = derivationline_entity.friction_factor
-        q = derivationline_entity.flow
-        hf = derivationline_entity.load_loss
-        lv = derivationline_entity.pipe_length
+        q = diameter_entity.demand_flow
+        s_max = diameter_entity.speed_max
 
-        theoretical_dimensions = (((constant_derivationline * f * (q ** 2)) / hf) * lv) ** Decimal(0.2)
+        theoretical_dimensions = (((4*q) / (pi*s_max))**Decimal(0.5)) * Decimal(1000.0)
 
         return ConsultNominalDiameterTable.nominal_diameter(float(theoretical_dimensions))
+    
+    @staticmethod
+    def calculate_loadloass_derivationline(loadloss_entity: DerivationlineLoadLoassEntity) -> Decimal:
+        
+        g = HydraulicConstants.gravity
+
+        l = loadloss_entity.length_derivationline
+        d = loadloss_entity.diameter_derivationline
+        q = loadloss_entity.flow
+        n = loadloss_entity.n_outputs
+
+        v = AuxCalculationHydraulicDesign.speed_water(q, d)
+        re = AuxCalculationHydraulicDesign.n_reynolds(v, d)
+        friction = AuxCalculationHydraulicDesign.friction_factor(d, re)
+        f_factor = AuxCalculationHydraulicDesign.f_factor(n)
+
+        hf = friction * (l / d) * ((v**2) /(2*g))
+
+        hf_corr = hf * f_factor
+
+        return hf_corr
