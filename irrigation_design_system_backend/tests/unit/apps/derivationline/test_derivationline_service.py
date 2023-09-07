aeec0b2757fd93aaa403446fb53307d34496ffdf
Lucas Lira <lucasclira@outlook.com>
2023-09-07 18:41:54 -0300

    fix: correction test derivation line

--- irrigation_design_system_backend/tests/unit/apps/derivationline/test_derivationline_service.py
+++ irrigation_design_system_backend/tests/unit/apps/derivationline/test_derivationline_service.py
@@ -1,23 +1,18 @@
 from _decimal import Decimal
 
-from apps.derivationline.derivationline_calculate_service import DerivationlineService
-from core.domain.entity.derivationline_entity import DerivationlineEntity
+from apps.hydraulic_design.derivationline.derivationline_calculate_service import DerivationlineService
+from core.domain.entity.derivationline_entity import DerivationlineDiameterEntity
 
 class TestDerivationlineService:
 
   def test_calculate_diameter(self):
 
-    error = 0
-
-    test_derivationline_entity = DerivationlineEntity(
-      friction_factor=Decimal(7),
-      flow=Decimal(25.0),
-      load_loss=Decimal(20.0),
-      pipe_length=Decimal(30.0),
+    test_derivationline_entity = DerivationlineDiameterEntity(
+        demand_flow = Decimal(0.008481),
+        speed_max = Decimal(1.5),
       )   
-    expected_result = float(35.0)
+    expected_result = float(100)
 
     result = DerivationlineService.calculate_derivationline_dimensions(test_derivationline_entity)
 
     assert (expected_result == result)
-    print(f'_____Test_Passed____\n')
