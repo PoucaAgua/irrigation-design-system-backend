6d15cd8ee3feac0ee97cdc0ab5d02cd99a914eb7
Lucas Lira <lucasclira@outlook.com>
2023-09-13 22:06:06 -0300

    test: add unit test for loadloss service

--- irrigation_design_system_backend/tests/unit/apps/derivationline/test_derivationline_service.py
+++ irrigation_design_system_backend/tests/unit/apps/derivationline/test_derivationline_service.py
@@ -1,18 +1,36 @@
 from _decimal import Decimal
 
 from apps.hydraulic_design.derivationline.derivationline_calculate_service import DerivationlineService
-from core.domain.entity.derivationline_entity import DerivationlineDiameterEntity
+from core.domain.entity.derivationline_entity import DerivationlineDiameterEntity, DerivationlineLoadLoassEntity
 
 class TestDerivationlineService:
 
   def test_calculate_diameter(self):
 
+    error = Decimal('1e-3')
+
     test_derivationline_entity = DerivationlineDiameterEntity(
         demand_flow = Decimal(0.008481),
         speed_max = Decimal(1.5),
       )   
-    expected_result = float(100)
+    expected = float(100)
 
     result = DerivationlineService.calculate_derivationline_dimensions(test_derivationline_entity)
 
-    assert (expected_result == result)
+    assert (expected == result)
+
+  def calculate_loadloass_derivationline(self):
+    
+    test_loadloss_entity = DerivationlineLoadLoassEntity(
+        length_derivationline = Decimal(50.0),
+        diameter_derivationline = Decimal(0.1),
+        flow = Decimal(1.079834458),
+        n_outputs = Decimal(62.5)
+    )
+    expected = Decimal(0.19613492804)
+    result = DerivationlineService.calculate_loadloass_derivationline(test_loadloss_entity)
+
+    assert abs(expected - result) <= self.error
+
+
+
