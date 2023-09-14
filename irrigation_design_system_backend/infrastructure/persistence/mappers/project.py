5ecf2619bf270d317aa7ebb0824df67d56d646f9
evandrosystems <evandrosystems@gmail.com>
2023-09-14 17:33:36 -0300

    feature: create the upsert endpoint to project, lateral_line and derivation_lite

--- irrigation_design_system_backend/infrastructure/persistence/mappers/project.py
+++
@@ -1,8 +0,0 @@
-from core.domain.entity.project_entity import ProjectEntity
-from infrastructure.persistence.models import Project
-
-
-class ProjectMapper:
-    @staticmethod
-    def entity_to_model(project_entity: ProjectEntity) -> Project:
-        return Project(**project_entity.dict())
