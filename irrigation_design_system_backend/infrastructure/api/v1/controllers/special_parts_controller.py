d6e97df078953047674ddc22d2bc33f1326ba3f0
Lucas Lira <lucasclira@outlook.com>
2023-09-16 09:36:29 -0300

    feat: create controller of special parts

---
+++ irrigation_design_system_backend/infrastructure/api/v1/controllers/special_parts_controller.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+from apps.special_parts.special_parts_service import SpecialPartsService
+from core.domain.entity.special_parts_entity import SpecialPartsEntity
+from infrastructure.api.v1.responses.special_parts import SpecialPartsReponse
+
+
+router = APIRouter()
+
+@router.post("/special_parts")
+def percent_wetted_area_irrigation_by_tree(input: SpecialPartsEntity):
+    specialparts = SpecialPartsService.special_parts_loadloss(input)
+    return SpecialPartsReponse(value=specialparts)
