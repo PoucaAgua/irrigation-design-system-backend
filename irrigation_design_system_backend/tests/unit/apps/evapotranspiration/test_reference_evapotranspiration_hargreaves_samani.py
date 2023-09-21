e468585d7dcda4ea92f1d5774523a94a2c80aaa6
ravellys <ravellyspyrrho@gmail.com>
2023-09-21 19:50:23 -0300

    fix tests

--- irrigation_design_system_backend/tests/unit/apps/evapotranspiration/test_reference_evapotranspiration_hargreaves_samani.py
+++ irrigation_design_system_backend/tests/unit/apps/evapotranspiration/test_reference_evapotranspiration_hargreaves_samani.py
@@ -2,10 +2,10 @@ from _decimal import Decimal
 
 import pytest
 
-from apps.evapotranspiration.funcitions_to_reference_evapotranspiration import (
+from apps.evapotranspiration.reference_evapotranspiration.hargreaves_samani_method import (
     calculate_radiation_hargreaves_samani,
 )
-from core.domain.enum.month import MonthEnum_hargraves
+from core.domain.enum.month import MonthEnum
 
 
 class TestReferenceEvapotranspirationHargreavesSamani:
@@ -14,34 +14,34 @@ class TestReferenceEvapotranspirationHargreavesSamani:
     @pytest.mark.parametrize(
         "latitude, month, expected_result",
         [
-            (Decimal("69.0"), MonthEnum_hargraves.Jan, Decimal("41.2")),
-            (Decimal("69.0"), MonthEnum_hargraves.Fev, Decimal("28.95")),
-            (Decimal("69.0"), MonthEnum_hargraves.Mar, Decimal("16.35")),
-            (Decimal("69.0"), MonthEnum_hargraves.Abr, Decimal("5.45")),
-            (Decimal("69.0"), MonthEnum_hargraves.Maio, Decimal("0.5")),
-            (Decimal("69.0"), MonthEnum_hargraves.Jun, Decimal("0")),
-            (Decimal("69.0"), MonthEnum_hargraves.Jul, Decimal("0")),
-            (Decimal("69.0"), MonthEnum_hargraves.Ago, Decimal("2.7")),
-            (Decimal("69.0"), MonthEnum_hargraves.Set, Decimal("11.3")),
-            (Decimal("69.0"), MonthEnum_hargraves.Out, Decimal("23.95")),
-            (Decimal("69.0"), MonthEnum_hargraves.Nov, Decimal("37.35")),
-            (Decimal("69.0"), MonthEnum_hargraves.Dez, Decimal("45")),
-            (Decimal("67.0"), MonthEnum_hargraves.Jan, Decimal("41.2")),
-            (Decimal("67.0"), MonthEnum_hargraves.Fev, Decimal("28.75")),
-            (Decimal("67.0"), MonthEnum_hargraves.Mar, Decimal("16.35")),
-            (Decimal("67.0"), MonthEnum_hargraves.Abr, Decimal("5.45")),
-            (Decimal("67.0"), MonthEnum_hargraves.Maio, Decimal("0.5")),
-            (Decimal("67.0"), MonthEnum_hargraves.Jun, Decimal("0")),
-            (Decimal("67.0"), MonthEnum_hargraves.Jul, Decimal("0")),
-            (Decimal("67.0"), MonthEnum_hargraves.Ago, Decimal("2.7")),
-            (Decimal("67.0"), MonthEnum_hargraves.Set, Decimal("11.3")),
-            (Decimal("67.0"), MonthEnum_hargraves.Out, Decimal("23.75")),
-            (Decimal("67.0"), MonthEnum_hargraves.Nov, Decimal("37.35")),
-            (Decimal("67.0"), MonthEnum_hargraves.Dez, Decimal("45")),
+            (Decimal("69.0"), MonthEnum.Jan, Decimal("41.2")),
+            (Decimal("69.0"), MonthEnum.Fev, Decimal("28.95")),
+            (Decimal("69.0"), MonthEnum.Mar, Decimal("16.35")),
+            (Decimal("69.0"), MonthEnum.Abr, Decimal("5.45")),
+            (Decimal("69.0"), MonthEnum.Mai, Decimal("0.5")),
+            (Decimal("69.0"), MonthEnum.Jun, Decimal("0")),
+            (Decimal("69.0"), MonthEnum.Jul, Decimal("0")),
+            (Decimal("69.0"), MonthEnum.Ago, Decimal("2.7")),
+            (Decimal("69.0"), MonthEnum.Set, Decimal("11.3")),
+            (Decimal("69.0"), MonthEnum.Out, Decimal("23.95")),
+            (Decimal("69.0"), MonthEnum.Nov, Decimal("37.35")),
+            (Decimal("69.0"), MonthEnum.Dez, Decimal("45")),
+            (Decimal("67.0"), MonthEnum.Jan, Decimal("41.2")),
+            (Decimal("67.0"), MonthEnum.Fev, Decimal("28.75")),
+            (Decimal("67.0"), MonthEnum.Mar, Decimal("16.35")),
+            (Decimal("67.0"), MonthEnum.Abr, Decimal("5.45")),
+            (Decimal("67.0"), MonthEnum.Mai, Decimal("0.5")),
+            (Decimal("67.0"), MonthEnum.Jun, Decimal("0")),
+            (Decimal("67.0"), MonthEnum.Jul, Decimal("0")),
+            (Decimal("67.0"), MonthEnum.Ago, Decimal("2.7")),
+            (Decimal("67.0"), MonthEnum.Set, Decimal("11.3")),
+            (Decimal("67.0"), MonthEnum.Out, Decimal("23.75")),
+            (Decimal("67.0"), MonthEnum.Nov, Decimal("37.35")),
+            (Decimal("67.0"), MonthEnum.Dez, Decimal("45")),
         ],
     )
     def test_calculate_radiation_hargreaves_samani(
-        self, latitude: Decimal, month: MonthEnum_hargraves, expected_result: Decimal
+        self, latitude: Decimal, month: MonthEnum, expected_result: Decimal
     ):
         # When
         result = calculate_radiation_hargreaves_samani(latitude, month)
