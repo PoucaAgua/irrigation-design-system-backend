74dab6c087825c86001ccbaff11b181407fe0450
JeanFirmino <albitzalcazar@protonmail.com>
2023-11-01 16:31:17 -0300

    Varredura com Flake e Black

--- irrigation_design_system_backend/infrastructure/api/v1/controllers/crop_coefficient_controller.py
+++ irrigation_design_system_backend/infrastructure/api/v1/controllers/crop_coefficient_controller.py
@@ -8,18 +8,18 @@ from infrastructure.api.v1.responses.crop_coefficient import (
     CropCoefficientDeleteResponse,
 )
 from core.domain.entity.crop_coefficient_entity import CropCoefficientEntity
-from infrastructure.persistence.repository.crop_coefficient_repository import CropCoefficientRepository
+from infrastructure.persistence.repository.crop_coefficient_repository import (
+    CropCoefficientRepository,
+)
 from infrastructure.persistence.session import get_db
 
 router = APIRouter()
 
+
 @router.post("/crop_coefficients/", response_model=CropCoefficientCreateResponse)
-def create_crop_coefficient(
-    crop_coefficient: CropCoefficientEntity,
-    db: Session = Depends(get_db)
-):
+def create_crop_coefficient(crop_coefficient: CropCoefficientEntity, db: Session = Depends(get_db)):
     crop_coefficient_repository = CropCoefficientRepository(db)
-    db_crop_coefficient = crop_coefficient_repository.create_crop_coefficient(
+    crop_coefficient_repository.create_crop_coefficient(
         crop_coefficient.crop_name,
         crop_coefficient.crop_type,
         crop_coefficient.kc_initial,
@@ -27,6 +27,7 @@ def create_crop_coefficient(
         crop_coefficient.kc_final,
     )
     return {"message": "Crop coefficient created successfully"}
+
 
 @router.get("/crop_coefficients/{crop_coefficient_id}/", response_model=CropCoefficientResponse)
 def read_crop_coefficient(crop_coefficient_id: int, db: Session = Depends(get_db)):
@@ -36,6 +37,7 @@ def read_crop_coefficient(crop_coefficient_id: int, db: Session = Depends(get_db
         raise HTTPException(status_code=404, detail="Crop coefficient not found")
     return db_crop_coefficient
 
+
 @router.get("/crop_coefficients/", response_model=CropCoefficientListResponse)
 def read_crop_coefficients(db: Session = Depends(get_db)):
     crop_coefficient_repository = CropCoefficientRepository(db)
@@ -43,11 +45,11 @@ def read_crop_coefficients(db: Session = Depends(get_db)):
     return {"crop_coefficients": crop_coefficients}
 
 
-@router.put("/crop_coefficients/{crop_coefficient_id}/", response_model=CropCoefficientUpdateResponse)
+@router.put(
+    "/crop_coefficients/{crop_coefficient_id}/", response_model=CropCoefficientUpdateResponse
+)
 def update_crop_coefficient(
-        crop_coefficient_id: int,
-        crop_coefficient: CropCoefficientEntity,
-        db: Session = Depends(get_db)
+    crop_coefficient_id: int, crop_coefficient: CropCoefficientEntity, db: Session = Depends(get_db)
 ):
     crop_coefficient_repository = CropCoefficientRepository(db)
     db_crop_coefficient = crop_coefficient_repository.get_crop_coefficient(crop_coefficient_id)
@@ -69,7 +71,9 @@ def update_crop_coefficient(
     return {"message": "Crop coefficient updated successfully"}
 
 
-@router.delete("/crop_coefficients/{crop_coefficient_id}/", response_model=CropCoefficientDeleteResponse)
+@router.delete(
+    "/crop_coefficients/{crop_coefficient_id}/", response_model=CropCoefficientDeleteResponse
+)
 def delete_crop_coefficient(crop_coefficient_id: int, db: Session = Depends(get_db)):
     crop_coefficient_repository = CropCoefficientRepository(db)
     success = crop_coefficient_repository.delete_crop_coefficient(crop_coefficient_id)
--- irrigation_design_system_backend/infrastructure/persistence/repository/crop_coefficient_repository.py
+++ irrigation_design_system_backend/infrastructure/persistence/repository/crop_coefficient_repository.py
@@ -7,20 +7,47 @@ class CropCoefficientRepository:
     def __init__(self, session: Session):
         self.session = session
 
-    def create_crop_coefficient(self, crop_name: str, crop_type: str, kc_initial: float, kc_mid_season: float, kc_final: float) -> models.CropCoefficientModel:
-        db_crop_coefficient = models.CropCoefficientModel(crop_name=crop_name, crop_type=crop_type, kc_initial=kc_initial, kc_mid_season=kc_mid_season, kc_final=kc_final)
+    def create_crop_coefficient(
+        self,
+        crop_name: str,
+        crop_type: str,
+        kc_initial: float,
+        kc_mid_season: float,
+        kc_final: float,
+    ) -> models.CropCoefficientModel:
+        db_crop_coefficient = models.CropCoefficientModel(
+            crop_name=crop_name,
+            crop_type=crop_type,
+            kc_initial=kc_initial,
+            kc_mid_season=kc_mid_season,
+            kc_final=kc_final,
+        )
         self.session.add(db_crop_coefficient)
         self.session.commit()
         self.session.refresh(db_crop_coefficient)
         return db_crop_coefficient
 
-    def get_crop_coefficient(self, crop_coefficient_id: int) -> Optional[models.CropCoefficientModel]:
-        return self.session.query(models.CropCoefficientModel).filter(models.CropCoefficientModel.id == crop_coefficient_id).first()
+    def get_crop_coefficient(
+        self, crop_coefficient_id: int
+    ) -> Optional[models.CropCoefficientModel]:
+        return (
+            self.session.query(models.CropCoefficientModel)
+            .filter(models.CropCoefficientModel.id == crop_coefficient_id)
+            .first()
+        )
 
     def get_all_crop_coefficients(self) -> List[models.CropCoefficientModel]:
         return self.session.query(models.CropCoefficientModel).all()
 
-    def update_crop_coefficient(self, crop_coefficient_id: int, crop_name: str, crop_type: str, kc_initial: float, kc_mid_season: float, kc_final: float) -> Optional[models.CropCoefficientModel]:
+    def update_crop_coefficient(
+        self,
+        crop_coefficient_id: int,
+        crop_name: str,
+        crop_type: str,
+        kc_initial: float,
+        kc_mid_season: float,
+        kc_final: float,
+    ) -> Optional[models.CropCoefficientModel]:
         db_crop_coefficient = self.get_crop_coefficient(crop_coefficient_id)
         if db_crop_coefficient:
             db_crop_coefficient.crop_name = crop_name
