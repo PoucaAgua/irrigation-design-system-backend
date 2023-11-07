3725db147eb3dba7eb64ad6bd17b4bec36e57eaa
Josafá melo <josafa.melo@camtecnologia.com.br>
2023-11-07 20:14:31 -0300

    feat: create tests  lateral line

---
+++ irrigation_design_system_backend/tests/unit/apps/lateral_line/test_lateral_line_service.py
@@ -0,0 +1,33 @@
+from _decimal import Decimal
+
+from apps.hydraulic_design.calculate_lateral_line.calculate_lateral_line import (
+    LateralLineService
+)
+from core.domain.entity.lateral_line_entity import (
+    LateralLineInput,
+)
+
+
+class TestDerivationLineService:
+
+    def test_calculate_diameter(self):
+        error = Decimal("1e-3")
+
+        test_lateral_line_entity = LateralLineInput(
+            internal_diameter=Decimal(0.016),
+            service_pressure=Decimal(10),
+            nominal_flow_rate=Decimal(1.6),
+            max_flow_rate_variation=Decimal(0.05),
+            emitter_spacing=Decimal(0.2),
+            flow_exponent=Decimal(0.5),
+            exponent_pressure_loss_equation=Decimal(1.75),
+            coefficient=Decimal(8.87295e-7),
+        )
+
+        expected = Decimal(61.06133924)
+
+        result = LateralLineService.calculate_length_lateral_line(test_lateral_line_entity)
+
+        assert abs(expected - result) < self.error
+
+
---
+++ irrigation_design_system_backend/tests/unit/apps/lateral_line/test_loadloss_lateral_line.py
@@ -0,0 +1,26 @@
+from _decimal import Decimal
+
+from apps.hydraulic_design.calculate_lateral_line.calculate_lateral_line import (
+    LateralLineService
+)
+from core.domain.entity.lateral_line_entity import (
+    LateralLineInput
+)
+
+class TestDerivationLineService:
+
+    def test_calculate_head_loss(self):
+        error = Decimal("1e-3")
+
+        test_lateral_line_entity = LateralLineInput(
+            friction_factor = Decimal(0.030),
+            water_velocity_lateral_line = Decimal(0.6748),
+            factor_f = Decimal(0.36),
+            gravity_acceleration = Decimal(9.81 )
+        )
+
+        expected = Decimal(2.260)
+
+        result = LateralLineService.calculate_head_loss(test_lateral_line_entity)
+
+        assert abs(expected - result) < self.error
