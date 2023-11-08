13bee3e9b285dce059f236dbb705c4598a40d273
Lucas Lira <lucasclira@outlook.com>
2023-11-07 21:42:23 -0300

    feat: create function load special parts table

--- irrigation_design_system_backend/core/tables/load_dataframe.py
+++ irrigation_design_system_backend/core/tables/load_dataframe.py
@@ -7,6 +7,11 @@ class LoadDataframe:
     def commercial_diameter(self):
         data = SourceTable.reference_table_reading("/files/reference_table_Commercial_diameters.csv")
         return data
+    
+    @property
+    def special_parts(self):
+        data = SourceTable.reference_table_reading("/files/special_parts_table.csv")
+        return data
 
 
 load_dataframes = LoadDataframe()
