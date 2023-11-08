31300a59cda3e3b825f306390c754f34efdc8bf5
Lucas Lira <lucasclira@outlook.com>
2023-11-07 23:59:55 -0300

    feat: add method for consult special parts

--- irrigation_design_system_backend/apps/hydraulic_design/special_parts/special_parts_service.py
+++ irrigation_design_system_backend/apps/hydraulic_design/special_parts/special_parts_service.py
@@ -2,13 +2,10 @@ from _decimal import Decimal
 
 from core.domain.entity.special_parts_input import SpecialPartsInput
 from core.domain.enum.special_parts_types import SpecialPartsTypes
-
+from apps.hydraulic_design.consult_special_parts.consult_loadloss_specialparts import ConsultLoadLossSpecialPartsTable
 
 class SpecialPartsService:
 
-    @classmethod
-    def __calculate_load_loss_by_special_part(cls, special_part: SpecialPartsTypes, diameter: Decimal) -> Decimal:
-        return Decimal('1')
 
     @classmethod
     def special_parts_load_loss(cls, special_parts_input: SpecialPartsInput) -> Decimal:
@@ -17,7 +14,7 @@ class SpecialPartsService:
 
         total_load_loss = 0
         for special_part in special_parts:
-            unit_load_loss_by_special_part = cls.__calculate_load_loss_by_special_part(special_part.type, diameter)
+            unit_load_loss_by_special_part = ConsultLoadLossSpecialPartsTable.loadloss_special_parts(special_part.type.value, diameter)
             total_load_loss += unit_load_loss_by_special_part
 
         return Decimal(total_load_loss)
