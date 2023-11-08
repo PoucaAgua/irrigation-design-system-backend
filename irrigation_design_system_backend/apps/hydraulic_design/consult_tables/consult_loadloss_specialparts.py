3f7d33b8f05b1ce8702b13a083d79e5d7a21fbc9
Lucas Lira <lucasclira@outlook.com>
2023-11-07 23:19:54 -0300

    feat: create consult table of special parts

---
+++ irrigation_design_system_backend/apps/hydraulic_design/consult_tables/consult_loadloss_specialparts.py
@@ -0,0 +1,12 @@
+from core.tables.load_dataframe import load_dataframes
+
+class ConsultLoadLossSpecialPartsTable:
+    
+    data = load_dataframes.special_parts
+
+    @classmethod
+    def loadloss_special_parts(cls, type, diameter):
+        
+        sp_load_loss = cls.data.loc[cls.data['D'] == diameter, type].values[0]
+        
+        return sp_load_loss
