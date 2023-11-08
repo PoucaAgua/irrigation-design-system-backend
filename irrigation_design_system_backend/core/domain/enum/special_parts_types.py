2f1e2cd5ae8f6be1621c7adcc3a93c2576bd88e5
Lucas Lira <lucasclira@outlook.com>
2023-11-07 23:20:30 -0300

    feat: add others types of table special parts

--- irrigation_design_system_backend/core/domain/enum/special_parts_types.py
+++ irrigation_design_system_backend/core/domain/enum/special_parts_types.py
@@ -5,3 +5,21 @@ class SpecialPartsTypes(Enum):
     JOELHO_90 = "JOELHO_90"
     JOELHO_45 = "JOELHO_45"
     # TODO input the others and translate the KEY to english
+    '''['DE', 
+    'D', 
+    'joelho_90', 
+    'joelho_45', 
+    'curva_90', 
+    'curva_45',
+    'tê_90_passagem_direta', 
+    'tê_90_saida_de_lado', 
+    'tê_90_saida_bilateral',
+    'entrada_normal', 
+    'entrada_de_borda', 
+    'saida_canalização',
+    'valvula_pe_crivo',
+    'valvula_retenção_tipo_leve',
+    'valvula_retenção_tipo_pesado', 
+    'registro_globo_aberto',
+    'registro_gaveta_aberto', 
+    'registro_angulo_aberto']'''
