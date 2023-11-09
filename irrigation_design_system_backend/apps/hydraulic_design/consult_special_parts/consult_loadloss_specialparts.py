af08bb5ffe82305254724e74c150661377e4c325
Lucas Lira <lucasclira@outlook.com>
2023-11-09 16:06:18 -0300

    refactor: improve error handling in consult

--- irrigation_design_system_backend/apps/hydraulic_design/consult_special_parts/consult_loadloss_specialparts.py
+++ irrigation_design_system_backend/apps/hydraulic_design/consult_special_parts/consult_loadloss_specialparts.py
@@ -6,7 +6,13 @@ class ConsultLoadLossSpecialPartsTable:
 
     @classmethod
     def loadloss_special_parts(cls, type, diameter):
+
+        if diameter not in cls.data['Diameter'].values:
+            raise ValueError('Diameter not found in reference tables')
         
+        if type not in cls.data.columns:
+            raise ValueError('Special part not found in reference tables')
+            
         sp_load_loss = cls.data.loc[cls.data['Diameter'] == diameter, type].values[0]
         
         return sp_load_loss
