94c681a38e080da870a03ccfbc15923b518b7464
Lucas Lira <lucasclira@outlook.com>
2023-11-09 16:10:05 -0300

    fix: corrected unit load loss calculation

--- irrigation_design_system_backend/apps/hydraulic_design/special_parts/special_parts_service.py
+++ irrigation_design_system_backend/apps/hydraulic_design/special_parts/special_parts_service.py
@@ -1,7 +1,6 @@
 from _decimal import Decimal
 
 from core.domain.entity.special_parts_input import SpecialPartsInput
-from core.domain.enum.special_parts_types import SpecialPartsTypes
 from apps.hydraulic_design.consult_special_parts.consult_loadloss_specialparts import ConsultLoadLossSpecialPartsTable
 
 class SpecialPartsService:
@@ -14,7 +13,7 @@ class SpecialPartsService:
 
         total_load_loss = 0
         for special_part in special_parts:
-            unit_load_loss_by_special_part = ConsultLoadLossSpecialPartsTable.loadloss_special_parts(special_part.type.value, diameter)
+            unit_load_loss_by_special_part = special_part.quantity * Decimal(ConsultLoadLossSpecialPartsTable.loadloss_special_parts(special_part.type.value, diameter))
             total_load_loss += unit_load_loss_by_special_part
 
         return Decimal(total_load_loss)
