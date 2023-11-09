7ab755d3f169ff118b82171e136fa536432f5460
Lucas Lira <lucasclira@outlook.com>
2023-11-09 16:11:07 -0300

    refactor: improved test implementation

--- irrigation_design_system_backend/tests/unit/apps/consult_special_parts/test_consult_special_parts.py
+++ irrigation_design_system_backend/tests/unit/apps/consult_special_parts/test_consult_special_parts.py
@@ -1,19 +1,20 @@
-from _decimal import Decimal
 import pytest
 
 from apps.hydraulic_design.consult_special_parts.consult_loadloss_specialparts import ConsultLoadLossSpecialPartsTable
 
 class TestConsultNominalDiameterTable:
     @pytest.mark.parametrize("diameter, types, expected", [
-        (0.50, 'joelho_90', 1.1),
-        (0.75, 'curva_45', 0.3),
-        (1.00, 'tê_90_saida_bilateral', 3.1),
-        (1.25, 'valvula_retenção_tipo_leve', 4.9),
-        (1.50, 'registro_angulo_aberto', 17)
+        (0.50, 'Degree_90_Elbow', 1.1),
+        (0.75, 'Degree_45_Bend', 0.3),
+        (1.00, 'Degree_90_Double_Branch_Tee', 3.1),
+        (1.25, 'Lightweight_Check_Valv', 'error'),
+        (0.330, 'Open_Angle_Valve', 'error'),
+        (5.223, 'Degree_90_Elbow', 'error')
 
     ])
     def test_loadloss_special_parts(self, diameter, types,expected):
-        assert (
-            Decimal(ConsultLoadLossSpecialPartsTable.loadloss_special_parts(types, diameter))
-            == Decimal(expected)
-        )
+        if expected == 'error':
+            with pytest.raises(ValueError):
+                ConsultLoadLossSpecialPartsTable.loadloss_special_parts(types, diameter)
+        else:
+            assert ConsultLoadLossSpecialPartsTable.loadloss_special_parts(types, diameter) == expected
