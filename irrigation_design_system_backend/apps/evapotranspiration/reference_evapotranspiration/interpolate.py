6ddfbad561989abc716eafe8f68f67976981fedb
Lucas Ravellys <57733801+ravellys@users.noreply.github.com>
2023-11-09 18:55:50 -0300

    removing scipy dependency (#75)

--- irrigation_design_system_backend/apps/evapotranspiration/reference_evapotranspiration/interpolate.py
+++ irrigation_design_system_backend/apps/evapotranspiration/reference_evapotranspiration/interpolate.py
@@ -1,7 +1,21 @@
 import pandas as pd
-from scipy.interpolate import interp1d
+import numpy as np
 
 
 def interpolate(x: pd.Series, y: pd.Series, value: float) -> float:
-    interp_func = interp1d(x, y, kind="linear", fill_value="extrapolate")
-    return interp_func(value).tolist()
+    sorted_idx = x.argsort()
+    x = x[sorted_idx]
+    y = y[sorted_idx]
+
+    idx = np.searchsorted(x, value)
+    if idx == 0:
+        slope = (y.iloc[1] - y.iloc[0]) / (x.iloc[1] - x.iloc[0])
+        return y.iloc[0] + slope * (value - x.iloc[0])
+    elif idx == len(x):
+        slope = (y.iloc[-1] - y.iloc[-2]) / (x.iloc[-1] - x.iloc[-2])
+        return y.iloc[-1] + slope * (value - x.iloc[-1])
+    else:
+        x1, x2 = x.iloc[idx - 1], x.iloc[idx]
+        y1, y2 = y.iloc[idx - 1], y.iloc[idx]
+        slope = (y2 - y1) / (x2 - x1)
+        return y1 + slope * (value - x1)
