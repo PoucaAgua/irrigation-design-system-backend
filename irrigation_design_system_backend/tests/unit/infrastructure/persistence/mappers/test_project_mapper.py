5d3c708b6bd5bf76c00079b0b911cd051815d986
Evandro Alves <evandrosystems@gmail.com>
2023-11-09 22:08:05 -0300

    updating tests project mapper

--- irrigation_design_system_backend/tests/unit/infrastructure/persistence/mappers/test_project_mapper.py
+++ irrigation_design_system_backend/tests/unit/infrastructure/persistence/mappers/test_project_mapper.py
@@ -7,66 +7,9 @@ from infrastructure.persistence.mappers.project_mapper import ProjectMapper, Der
 
 class TestProjectMapper:
 
-    def test_output_instance(self):
-
-        # input_data
-        project_input = ProjectInput(
-            id=1,
-            user_id=1,
-            group_id="1",
-            description="Tomato plantation",
-            status="draft",
-            crop="Tomato",
-            maximum_actual_irrigation_required=100,
-            crop_evapotranspiration=100,
-            total_irrigation_required=100,
-            derivation_line=[],
-            lateral_line=[],
-        )
-
-        # expected_output
-        project = ProjectMapper.model_from_input(project_input)
-
-        assert isinstance(project, Project)
-
-    """
-        Tested a valid mapper
-    """
-    def test_valid_mapping(self):
-
-        # input_data
-        project_input = ProjectInput(
-            id=1,
-            user_id=1,
-            group_id="1",
-            description="Tomato plantation",
-            status="draft",
-            crop="Tomato",
-            maximum_actual_irrigation_required=100,
-            crop_evapotranspiration=100,
-            total_irrigation_required=100,
-            derivation_line=[],
-            lateral_line=[]
-        )
-
-        # expected_output
-        project = ProjectMapper.model_from_input(project_input)
-
-        assert project.id == project_input.id
-        assert project.user_id == project_input.user_id
-        assert project.group_id == project_input.group_id
-        assert project.description == project_input.description
-        assert project.status == project_input.status
-        assert project.crop == project_input.crop
-        assert project.maximum_actual_irrigation_required == project_input.maximum_actual_irrigation_required
-        assert project.total_irrigation_required == project_input.total_irrigation_required
-        assert project.derivation_line == project_input.derivation_line
-        assert project.lateral_line == project_input.lateral_line
-
-    def test_output_lines(self):
-
-        # input_data
-        project_input = ProjectInput(
+    @pytest.fixture
+    def project_input(self):
+        return ProjectInput(
             id=1,
             user_id=1,
             group_id="1",
@@ -100,19 +43,77 @@ class TestProjectMapper:
             ]
         )
 
+    @pytest.fixture
+    def project_persisted(self):
+        return Project(
+            id=1,
+            user_id=1,
+            group_id="1",
+            description="Tomato plantation",
+            status="draft",
+            crop="Tomato",
+            maximum_actual_irrigation_required=100,
+            crop_evapotranspiration=100,
+            total_irrigation_required=100
+        )
+
+    def test_model_from_input_with_matching_type(self, project_input):
+
         # expected_output
         project = ProjectMapper.model_from_input(project_input)
 
-        assert isinstance(project.lateral_line[0], LateralLine)
-        assert isinstance(project.derivation_line[0], DerivationLine)
+        assert isinstance(project, Project)
+
+    def test_model_from_input_valid_mapping(self, project_input):
+
+        # expected_output
+        project = ProjectMapper.model_from_input(project_input)
+
+        assert project.id == project_input.id
+        assert project.user_id == project_input.user_id
+        assert project.group_id == project_input.group_id
+        assert project.description == project_input.description
+        assert project.status == project_input.status
+        assert project.crop == project_input.crop
+        assert project.maximum_actual_irrigation_required == project_input.maximum_actual_irrigation_required
+        assert project.total_irrigation_required == project_input.total_irrigation_required
+
+        for derivation_line_input, derivation_line in zip(project_input.derivation_line, project.derivation_line):
+            assert derivation_line_input.pipe_type == derivation_line.pipe_type
+            assert derivation_line_input.inlet_pressure == derivation_line.inlet_pressure
+            assert derivation_line_input.length == derivation_line.length
+            assert derivation_line_input.diameter == derivation_line.diameter
+            assert derivation_line_input.localized_loss == derivation_line.localized_loss
+            assert derivation_line_input.type == derivation_line.type
+
+        for lateral_line_input, lateral_line in zip(project_input.lateral_line, project.lateral_line):
+            assert lateral_line_input.dripper == lateral_line.dripper
+            assert lateral_line_input.decline == lateral_line.decline
+            assert lateral_line_input.inlet_pressure == lateral_line.inlet_pressure
+            assert lateral_line_input.separation_between_issuers == lateral_line.separation_between_issuers
+            assert lateral_line_input.length_max == lateral_line.length_max
+            assert lateral_line_input.diameter == lateral_line.diameter
+            assert lateral_line_input.localized_loss == lateral_line.localized_loss
+            assert lateral_line_input.type == lateral_line.type
+
+    def test_model_from_input_with_lines_matching_type(self, project_input):
+
+        # expected_output
+        output = ProjectMapper.model_from_input(project_input)
+
+        assert isinstance(output.lateral_line[0], LateralLine)
+        assert isinstance(output.derivation_line[0], DerivationLine)
+
+    def test_model_from_input_and_persisted_with_valid_inputs(self, project_input, project_persisted):
+        output = ProjectMapper.model_from_input_and_persisted(project_input, project_persisted)
+        assert isinstance(output, Project)
 
 
 class TestDerivationLineMapper:
 
-    def test_output_instance(self):
-
-        # input_data
-        line = DerivationLineInput(
+    @pytest.fixture
+    def derivation_line_input(self):
+        return DerivationLineInput(
             pipe_type="Iron",
             inlet_pressure=10,
             length="150",
@@ -121,21 +122,41 @@ class TestDerivationLineMapper:
             type="with_plc"
         )
 
-        # input_data
-        project_id = 1
+    @pytest.fixture
+    def derivation_lines_persisted(self):
+        return [
+            DerivationLine(
+                id=1,
+                project_id=1,
+                pipe_type="Iron",
+                inlet_pressure=10,
+                length="150",
+                diameter="100",
+                localized_loss="240",
+                type="with_plc"
+            )
+        ]
+
+    def test_from_input_with_matching_type(self, derivation_line_input):
 
         # expected_output
-        derivation_line = DerivationLineMapper.model_from_input(line, project_id)
+        output = DerivationLineMapper.model_from_input(line=derivation_line_input, project_id=1)
 
-        assert isinstance(derivation_line, DerivationLine)
+        assert isinstance(output, DerivationLine)
+
+    def test_model_from_input_and_persisted_with_matching_type(self, derivation_line_input, derivation_lines_persisted):
+        output = DerivationLineMapper.model_from_input_and_persisted(
+            derivation_line_input, derivation_lines_persisted, project_id=1
+        )
+
+        assert isinstance(output, DerivationLine)
 
 
 class TestLateralLineMapper:
 
-    def test_output_instance(self):
-
-        # input_data
-        line = LateralLineInput(
+    @pytest.fixture
+    def lateral_line_input(self):
+        return LateralLineInput(
             dripper="10mm",
             decline=10,
             inlet_pressure=10,
@@ -146,9 +167,32 @@ class TestLateralLineMapper:
             type="with_plc"
         )
 
-        # input_data
-        project_id = 1
+    @pytest.fixture
+    def lateral_lines_persisted(self):
+        return [
+            LateralLine(
+                id=1,
+                project_id=1,
+                dripper="10mm",
+                decline=10,
+                inlet_pressure=10,
+                separation_between_issuers=20,
+                length_max=6,
+                diameter=5,
+                localized_loss=100,
+                type="with_plc"
+            )
+        ]
 
-        lateral_line = LateralLineMapper.model_from_input(line, project_id)
+    def test_model_from_input_with_matching_type(self, lateral_line_input):
 
-        assert isinstance(lateral_line, LateralLine)
+        output = LateralLineMapper.model_from_input(line=lateral_line_input, project_id=1)
+
+        assert isinstance(output, LateralLine)
+
+    def test_model_from_input_and_persisted_with_matching_type(self, lateral_line_input, lateral_lines_persisted):
+        output = LateralLineMapper.model_from_input_and_persisted(
+            lateral_line_input, lateral_lines_persisted, project_id=1
+        )
+
+        assert isinstance(output, LateralLine)
