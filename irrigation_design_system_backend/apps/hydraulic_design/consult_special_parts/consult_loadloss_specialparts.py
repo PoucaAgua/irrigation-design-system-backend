02ec77029fb8bee6dbd9de8a432e16fdf1e292c9
Lucas Lira <lucasclira@outlook.com>
2023-11-11 10:16:05 -0300

    refactor: adjust loadloss_specialparts for PEP8

--- irrigation_design_system_backend/apps/hydraulic_design/consult_special_parts/consult_loadloss_specialparts.py
+++ irrigation_design_system_backend/apps/hydraulic_design/consult_special_parts/consult_loadloss_specialparts.py
@@ -1,18 +1,17 @@
 from core.tables.dataframes import dataframes
 
+
 class ConsultLoadLossSpecialPartsTable:
     
     data = dataframes.special_parts
 
     @classmethod
     def loadloss_special_parts(cls, type, diameter):
-
         if diameter not in cls.data['Diameter'].values:
             raise ValueError('Diameter not found in reference tables')
         
         if type not in cls.data.columns:
             raise ValueError('Special part not found in reference tables')
-            
-        sp_load_loss = cls.data.loc[cls.data['Diameter'] == diameter, type].values[0]
         
+        sp_load_loss = cls.data.loc[cls.data['Diameter'] == diameter, type].values[0]
         return sp_load_loss
