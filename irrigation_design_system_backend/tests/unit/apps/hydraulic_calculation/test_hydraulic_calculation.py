d9eb8e4154ce7bb100068505038dd89319c720a5
Josafá melo <josafa.melo@camtecnologia.com.br>
2023-11-12 00:22:41 -0300

    feat: create test loadloss and hydraulic

--- irrigation_design_system_backend/tests/unit/apps/hydraulic_calculation/test_hydraulic_calculation.py
+++ irrigation_design_system_backend/tests/unit/apps/hydraulic_calculation/test_hydraulic_calculation.py
@@ -14,9 +14,27 @@ class TestHydraulicCalculation:
 
         assert abs(result - expected) <= self.error
 
+    def test_speed_water_lateral_line(self):
+        result = Decimal(HydraulicCalculation.speed_water_lateral_line(0.00014, 0.0002))
+        expected = Decimal(0.7)
+
+        assert abs(result - expected) <= self.error
+
+    def test_flow_lateral_line(self):
+        result = Decimal(HydraulicCalculation.flow_lateral_line(305.307, 1.6))
+        expected = Decimal(488.4907139)
+
+        assert abs(result - expected) <= self.error
+
     def test_n_reynolds(self):
         result = Decimal(HydraulicCalculation.n_reynolds(1.07983445809, 0.1))
         expected = Decimal(107983.4458092)
+
+        assert abs(result - expected) <= self.error
+
+    def test_n_reynolds_lateral_line(self):
+        result = Decimal(HydraulicCalculation.n_reynolds(0.67487597, 0.016))
+        expected = Decimal(10798.01552)
 
         assert abs(result - expected) <= self.error
 
@@ -26,8 +44,41 @@ class TestHydraulicCalculation:
 
         assert abs(result - expected) <= self.error
 
+    def test_friction_factor_lateral_line(self):
+        result = Decimal(HydraulicCalculation.friction_factor(0.016, 10798.01552))
+        print(result)
+        expected = Decimal(0.030367585)
+
+        assert abs(result - expected) <= self.error
+
     def test_f_factor(self):
         result = Decimal(HydraulicCalculation.f_factor(62.5))
         expected = Decimal(0.371673314)
 
+        assert abs(result - expected) <= self.error
+
+    def test_f_factor_lateral_line(self):
+        result = Decimal(HydraulicCalculation.f_factor(305.307))
+        expected = Decimal(0.3652787007)
+
+        assert abs(result - expected) <= self.error
+
+    def test_coeficient_K(self):
+        result = HydraulicCalculation.coeficient_K(0.016)
+        expected = Decimal()
+        assert abs(result - expected) <= self.error
+
+    def test_emissors_number(self):
+        result = HydraulicCalculation.emissors_number(61.0613, 0.2)
+        expected = Decimal(305.307)
+        assert abs(result - expected) <= self.error
+
+    def test_flow_section_area(self):
+        result = HydraulicCalculation.flow_section_area(0.016)
+        expected = Decimal(0.0002)
+        assert abs(result - expected) <= self.error
+
+    def test_flow_lateral_line_conversion(self):
+        result = HydraulicCalculation.flow_lateral_line_conversion(488.4907139)
+        expected = Decimal(0.00014)
         assert abs(result - expected) <= self.error
--- irrigation_design_system_backend/tests/unit/apps/lateral_line/test_loadloss_lateral_line.py
+++ irrigation_design_system_backend/tests/unit/apps/lateral_line/test_loadloss_lateral_line.py
@@ -1,22 +1,17 @@
 from _decimal import Decimal
-
 from apps.hydraulic_design.calculate_lateral_line.calculate_lateral_line import LateralLineService
-from core.domain.entity.lateral_line_entity import LateralLineInput
+from core.domain.entity.lateral_line_entity import LateralLineHeadLossInput
 
 
 class TestLateralLineService:
     def test_calculate_head_loss(self):
         error = Decimal("1e-3")
-
-        test_lateral_line_entity = LateralLineInput(
-            friction_factor=Decimal(0.030),
-            water_velocity_lateral_line=Decimal(0.6748),
-            factor_f=Decimal(0.36),
-            gravity_acceleration=Decimal(9.81),
+        test_lateral_line_entity = LateralLineHeadLossInput(
+            length_lateral_line=Decimal(61.0613),
+            internal_diameter=Decimal(0.016),
+            emitter_spacing=Decimal(0.2),
+            nominal_flow_rate=Decimal(1.6),
         )
-
-        expected = Decimal(2.260)
-
+        expected = Decimal(0.982720192)
         result = LateralLineService.calculate_head_loss(test_lateral_line_entity)
-
         assert abs(expected - result) <= error
