81063db67f4f833ce17d69f7988ee4454501e9ae
ravellys <ravellyspyrrho@gmail.com>
2023-11-13 19:06:50 -0300

    creating tests to repository

--- irrigation_design_system_backend/tests/unit/infrastructure/persistence/repository/test_projects_repository.py
+++ irrigation_design_system_backend/tests/unit/infrastructure/persistence/repository/test_projects_repository.py
@@ -1,37 +1,55 @@
 import pytest
 from unittest.mock import Mock, patch
 
-from infrastructure.persistence.mappers.project_mapper import ProjectMapper
-from infrastructure.persistence.models.projects import Project
-from core.domain.entity.project_input import ProjectInput
 from infrastructure.persistence.repository.projects_repository import ProjectRepository
 
 
 class TestProjectRepository:
 
+    repository = ProjectRepository()
+
     @pytest.fixture
     def db(self):
         return Mock()
 
-    def test_upsert_insert(self, db):
+    @patch('infrastructure.persistence.repository.projects_repository.ProjectMapper')
+    def test_upsert_insert(self, project_mapper_mock, db):
+        # given
+        project_input = Mock()
+        project_db_mock = Mock(id=None)
+        project_mapper_mock.model_from_input.return_value = project_db_mock
 
-        project_input = ProjectInput(
-            id=1,
-            user_id="1",
-            group_id="1",
-            description="Lemon farming",
-            status="draft",
-            crop="Lemon",
-            maximum_actual_irrigation_required=100,
-            crop_evapotranspiration=100,
-            total_irrigation_required=500,
-            derivation_line=[],
-            lateral_line=[]
+        # when
+        result = self.repository.upsert(db=db, project_input=project_input)
+
+        # then
+        assert project_db_mock == result
+        project_mapper_mock.model_from_input.assert_called_with(project_input)
+        db.add.assert_called_with(project_db_mock)
+        db.commit.assert_called_once()
+        db.close.assert_called_once()
+
+    @patch('infrastructure.persistence.repository.projects_repository.ProjectMapper')
+    def test_upsert_update(self, project_mapper_mock, db):
+        # given
+        project_input = Mock()
+        project_db_mock = Mock(id=1)
+        project_db_persisted_mock = Mock(id=1)
+        project_db_mock_2 = Mock(id=1)
+
+        project_mapper_mock.model_from_input.return_value = project_db_mock
+        db.query().filter().first.return_value = project_db_persisted_mock
+        project_mapper_mock.model_from_input_and_persisted.return_value = project_db_mock_2
+
+        # when
+        result = self.repository.upsert(db=db, project_input=project_input)
+
+        # then
+        project_mapper_mock.model_from_input.assert_called_once_with(project_input)
+        project_mapper_mock.model_from_input_and_persisted.assert_called_once_with(
+            project_input, project_db_persisted_mock
         )
-        repository = ProjectRepository()
 
-        repository.upsert(project_input, db=db)
-
-        # Assert that the necessary methods were called
-        db.save.assert_called_with(Mock())
-
+        db.merge.assert_called_with(project_db_mock_2)
+        db.commit.assert_called_once()
+        db.close.assert_called_once()
