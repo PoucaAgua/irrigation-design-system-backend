92522583e65f4281e69744fc846a5311cd4e96a4
Josafá melo <josafa.melo@camtecnologia.com.br>
2023-11-13 22:17:13 -0300

    refactor: lateral line entity validations

--- irrigation_design_system_backend/core/domain/entity/lateral_line_entity.py
+++ irrigation_design_system_backend/core/domain/entity/lateral_line_entity.py
@@ -1,22 +1,24 @@
 from decimal import Decimal
-from dataclasses import dataclass
+from pydantic import BaseModel, Field
 
 
-@dataclass
-class LateralLineInput:
-    service_pressure: Decimal
-    nominal_flow_rate: Decimal
-    max_flow_rate_variation: Decimal
-    internal_diameter: Decimal
-    emitter_spacing: Decimal
-    flow_exponent: Decimal
-    exponent_pressure_loss_equation: Decimal
-    coefficient: Decimal
+class LateralLineInput(BaseModel):
+    service_pressure: Decimal = Field(..., ge=0, description="[SP] Service Pressure (mca)")
+    nominal_flow_rate: Decimal = Field(..., ge=0, description="[NFR] Nominal Flow Rate (L/h)")
+    max_flow_rate_variation: Decimal = Field(
+        ..., ge=0, description="[MFV] Max Flow Rate Variation (%)"
+    )
+    internal_diameter: Decimal = Field(..., ge=0, description="[ID] Internal Diameter (m)")
+    emitter_spacing: Decimal = Field(..., ge=0, description="[ES] Emitter Spacing (m)")
+    flow_exponent: Decimal = Field(..., ge=0, description="[FE] Flow Exponent (dimensionless)")
+    exponent_pressure_loss_equation: Decimal = Field(
+        ..., ge=0, description="[EPL] Exponent Pressure Loss Equation (dimensionless)"
+    )
+    coefficient: Decimal = Field(..., ge=0, description="[C] Coefficient (K)")
 
 
-@dataclass
-class LateralLineHeadLossInput:
-    length_lateral_line: Decimal
-    internal_diameter: Decimal
-    emitter_spacing: Decimal
-    nominal_flow_rate: Decimal
+class LateralLineHeadLossInput(BaseModel):
+    length_lateral_line: Decimal = Field(..., ge=0, description="[LL] Length Lateral Line (L)")
+    internal_diameter: Decimal = Field(..., ge=0, description="[ID] Internal Diameter (m)")
+    emitter_spacing: Decimal = Field(..., ge=0, description="[ES] Emitter Spacing (m)")
+    nominal_flow_rate: Decimal = Field(..., ge=0, description="[NFR] Nominal Flow Rate (L/h)")
