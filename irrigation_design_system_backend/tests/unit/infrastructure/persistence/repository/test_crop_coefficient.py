0133de41cbf4324bbbca4b1102f398117fa901ce
JeanFirmino <albitzalcazar@protonmail.com>
2023-11-20 14:26:39 -0300

    Correções dos testes unitários do mapper e repository

--- irrigation_design_system_backend/tests/unit/infrastructure/persistence/repository/test_crop_coefficient.py
+++ irrigation_design_system_backend/tests/unit/infrastructure/persistence/repository/test_crop_coefficient.py
@@ -2,67 +2,75 @@ import pytest
 from unittest.mock import Mock, patch
 from infrastructure.persistence.repository.crop_coefficient_repository import CropCoefficientRepository
 
+class CropCoefficientNotFoundError(Exception):
+    pass
 
-class TestCropCoefficientRepository:
+try:
+    class TestCropCoefficientRepository:
 
-    repository = CropCoefficientRepository()
+        repository = CropCoefficientRepository()
 
-    @pytest.fixture
-    def db(self):
-        return Mock()
+        @pytest.fixture
+        def db(self):
+            return Mock()
 
-    @patch('infrastructure.persistence.repository.crop_coefficient_repository.CropCoefficientMapper')
-    def test_upsert_insert(self, crop_coefficient_mapper_mock, db):
-        # given
-        coefficient_input = Mock(id=None)
-        coefficient_db_mock = Mock()
-        crop_coefficient_mapper_mock.entity_to_model.return_value = coefficient_db_mock
+        @patch('infrastructure.persistence.repository.crop_coefficient_repository.CropCoefficientMapper')
+        def test_upsert_insert(self, crop_coefficient_mapper_mock, db):
+            # given
+            coefficient_input = Mock(id=None)
+            coefficient_db_mock = Mock()
+            crop_coefficient_mapper_mock.entity_to_model.return_value = coefficient_db_mock
 
-        # when
-        result = self.repository.upsert(db=db, crop_coefficient=coefficient_input)
+            # when
+            result = self.repository.upsert(db=db, crop_coefficient=coefficient_input)
 
-        # then
-        assert coefficient_db_mock == result
-        crop_coefficient_mapper_mock.entity_to_model.assert_called_with(coefficient_input)
-        db.add.assert_called_with(coefficient_db_mock)
-        db.commit.assert_called_once()
-        db.close.assert_called_once()
+            # then
+            assert coefficient_db_mock == result
+            crop_coefficient_mapper_mock.entity_to_model.assert_called_with(coefficient_input)
+            db.add.assert_called_with(coefficient_db_mock)
+            db.commit.assert_called_once()
+            db.close.assert_called_once()
 
-    @patch('infrastructure.persistence.repository.crop_coefficient_repository.CropCoefficientMapper')
-    def test_upsert_update(self, crop_coefficient_mapper_mock, db):
-        # given
-        coefficient_input = Mock(id=1)
-        coefficient_db_mock = Mock()
-        coefficient_db_persisted_mock = Mock(id=1)
-        coefficient_db_mock_2 = Mock(id=1)
+        @patch('infrastructure.persistence.repository.crop_coefficient_repository.CropCoefficientMapper')
+        def test_upsert_update(self, crop_coefficient_mapper_mock, db):
+            # given
+            coefficient_input = Mock(id=1)
+            coefficient_db_mock = Mock()
+            coefficient_db_persisted_mock = Mock(id=1)
+            coefficient_db_mock_2 = Mock(id=1)
 
-        crop_coefficient_mapper_mock.entity_to_model.return_value = coefficient_db_mock
-        db.query().filter().first.return_value = coefficient_db_persisted_mock
-        crop_coefficient_mapper_mock.entity_to_model_persisted.return_value = coefficient_db_mock_2
+            crop_coefficient_mapper_mock.entity_to_model.return_value = coefficient_db_mock
+            db.query().filter().first.return_value = coefficient_db_persisted_mock
+            crop_coefficient_mapper_mock.entity_to_model_persisted.return_value = coefficient_db_mock_2
 
-        # when
-        result = self.repository.upsert(db=db, crop_coefficient=coefficient_input)
+            # when
+            result = self.repository.upsert(db=db, crop_coefficient=coefficient_input)
 
-        # then
-        assert coefficient_db_mock_2 == result
-        crop_coefficient_mapper_mock.entity_to_model_persisted.assert_called_once_with(
-            coefficient_input, coefficient_db_persisted_mock
-        )
+            # then
+            assert coefficient_db_mock_2 == result
+            crop_coefficient_mapper_mock.entity_to_model_persisted.assert_called_once_with(
+                coefficient_input, coefficient_db_persisted_mock
+            )
 
-        db.merge.assert_called_with(coefficient_db_mock_2)
-        db.commit.assert_called_once()
-        db.close.assert_called_once()
+            db.merge.assert_called_with(coefficient_db_mock_2)
+            db.commit.assert_called_once()
+            db.close.assert_called_once()
 
-    def test_upsert_invalid_id(self, db):
-        # given
-        coefficient_input = Mock(id=1)
-        db.query().filter().first.return_value = None
+        def test_upsert_invalid_id(self, db):
+            # given
+            coefficient_input = Mock(id=1)
+            db.query().filter().first.return_value = None
 
-        # when & then
-        with pytest.raises(FileNotFoundError) as exc_info:
-            self.repository.upsert(db=db, crop_coefficient=coefficient_input)
+            # when & then
+            with pytest.raises(FileNotFoundError) as exc_info:
+                self.repository.upsert(db=db, crop_coefficient=coefficient_input)
 
-        assert str(exc_info.value) == "invalid id 1"
-        db.merge.assert_not_called()
-        db.commit.assert_not_called()
-        db.close.assert_called_once()
+            assert str(exc_info.value) == "invalid id 1"
+            db.merge.assert_not_called()
+            db.commit.assert_not_called()
+            db.close.assert_called_once()
+
+except CropCoefficientNotFoundError as e:
+    print(f"Error: {e}")
+
+
