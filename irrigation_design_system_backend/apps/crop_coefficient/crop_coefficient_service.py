024f64f2235f2a278f8fdf56583ed1db0c7ad4a4
JeanFirmino <albitzalcazar@protonmail.com>
2024-01-08 12:10:54 -0300

    git id and git all

--- irrigation_design_system_backend/apps/crop_coefficient/crop_coefficient_service.py
+++ irrigation_design_system_backend/apps/crop_coefficient/crop_coefficient_service.py
@@ -11,3 +11,14 @@ class CropCoefficientService:
     def upsert_crop_coefficient(cls, crop_coefficient_input: CropCoefficientInput):
         crop_coefficient = cls.crop_coefficient_repository.upsert(crop_coefficient_input)
         return crop_coefficient
+
+    @classmethod
+    def get_crop_coefficient_by_id(cls, crop_id: int):
+        crop_coefficient = cls.crop_coefficient_repository.get_by_id(crop_id)
+        return crop_coefficient
+
+    @classmethod
+    def get_all_crop_coefficients(cls):
+        crop_coefficients = cls.crop_coefficient_repository.get_all()
+        return crop_coefficients
+
--- irrigation_design_system_backend/infrastructure/api/v1/controllers/crop_coefficient_controller.py
+++ irrigation_design_system_backend/infrastructure/api/v1/controllers/crop_coefficient_controller.py
@@ -1,10 +1,16 @@
-from fastapi import APIRouter
+from http.client import HTTPException
+from typing import List
+
+from fastapi import APIRouter, Depends
 from apps.crop_coefficient.crop_coefficient_service import CropCoefficientService
 from core.domain.entity.crop_coefficient_input import CropCoefficientInput
 from infrastructure.api.v1.responses.crop_coefficient_response import CropCoefficientResponse
+from infrastructure.persistence.session import get_db
+
+from infrastructure.persistence.repository.crop_coefficient_repository import \
+    CropCoefficientRepository
 
 router = APIRouter()
-
 
 @router.post("/crop_coefficients", response_model=CropCoefficientResponse)
 def create_or_update_crop_coefficient(crop_coefficient_entity: CropCoefficientInput):
@@ -25,3 +31,42 @@ def create_or_update_crop_coefficient(crop_coefficient_entity: CropCoefficientIn
         is_active=crop_coefficient.is_active,
         status=status_message,
     )
+
+@router.get("/crop_coefficients/{crop_id}", response_model=CropCoefficientResponse)
+def get_crop_coefficient_by_id(crop_id: int):
+    db = get_db()
+    crop_coefficient = CropCoefficientRepository().get_by_id(db, crop_id)
+    if not crop_coefficient:
+        raise HTTPException(status_code=404, detail="Crop coefficient not found")
+
+    return CropCoefficientResponse(
+        crop_id=crop_coefficient.crop_id,
+        crop_name=crop_coefficient.crop_name,
+        crop_type=crop_coefficient.crop_type,
+        kc_initial=crop_coefficient.kc_initial,
+        kc_mid_season=crop_coefficient.kc_mid_season,
+        kc_final=crop_coefficient.kc_final,
+        is_active=crop_coefficient.is_active,
+        status="Crop coefficient retrieved successfully"
+    )
+
+
+@router.get("/crop_coefficients", response_model=List[CropCoefficientResponse])
+def get_all_crop_coefficients(db=Depends(get_db)):
+    crop_coefficients = CropCoefficientService.get_all_crop_coefficients(db)
+    if not crop_coefficients:
+        raise HTTPException(status_code=404, detail="No crop coefficients found")
+
+    return [
+        CropCoefficientResponse(
+            crop_id=crop.crop_id,
+            crop_name=crop.crop_name,
+            crop_type=crop.crop_type,
+            kc_initial=crop.kc_initial,
+            kc_mid_season=crop.kc_mid_season,
+            kc_final=crop.kc_final,
+            is_active=crop.is_active,
+            status="Crop coefficient retrieved successfully"
+        )
+        for crop in crop_coefficients
+    ]
