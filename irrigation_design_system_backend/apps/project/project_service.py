9a176d70d87a69ad1dadf5a3a4e2f4a9ebb7a6ac
Evandro <evandrosystems@gmail.com>
2024-01-08 14:56:20 -0300

    formatting with black

--- irrigation_design_system_backend/apps/project/project_service.py
+++ irrigation_design_system_backend/apps/project/project_service.py
@@ -2,13 +2,14 @@ from core.domain.entity.project_input import ProjectInput
 
 
 class ProjectService:
-
     @classmethod
     def upsert_project(cls, project_input: ProjectInput):
         from infrastructure.persistence.repository.project_repository import ProjectRepository
+
         return ProjectRepository.upsert(project_input)
 
     @classmethod
     def get_all(cls, group_id: str, user_id: int):
         from infrastructure.persistence.repository.project_repository import ProjectRepository
+
         return ProjectRepository.get_all(group_id, user_id)
