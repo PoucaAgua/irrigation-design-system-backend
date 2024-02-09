b7bd3e46f320a4d608c45ceae9d3433ea9a07474
Evandro Alves <78321345+evandrosystems@users.noreply.github.com>
2024-02-09 17:44:17 -0300

    feature: install alembic (#89)
    * feat: install alembic
    
    * refactor: refactoring main.py
    
    * chore: configuring alembic
    
    * fix: fixing import
    
    * feat: create first migrations
    
    * feat: adding drop query to remove object types in downgrade migration

--- irrigation_design_system_backend/main.py
+++ irrigation_design_system_backend/main.py
@@ -1,21 +1,9 @@
 from fastapi import FastAPI
 from mangum import Mangum
-from sqlalchemy_utils import database_exists, create_database
 from fastapi.middleware.cors import CORSMiddleware
 
 from core.configs.settings import settings
 from infrastructure.api.v1 import router as api_router
-from infrastructure.persistence.models.base import Base
-from infrastructure.persistence.session import engine
-
-
-def create_database_if_not_exists():
-    if not database_exists(engine.url):
-        create_database(engine.url)
-
-
-def create_tables():
-    Base.metadata.create_all(bind=engine)
 
 
 def start_application():
@@ -32,8 +20,6 @@ def start_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
-    create_database_if_not_exists()
-    create_tables()
     app.include_router(api_router, prefix="/api/v1")
     return app
 
